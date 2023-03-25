b63a9573e6db32f3abe001816ab8fa93bc9f97c9
Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-03-25 16:33:37 +0700

    get user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -23,8 +23,17 @@ let postCRUD = async (req, res) => {
     return res.send('post crud from server');
 }
 
+let displayGetCRUD = async(req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log(data);
+    return res.render('displayCRUD.ejs',{
+        dataTable: data
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
-    postCRUD: postCRUD
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD
 }
